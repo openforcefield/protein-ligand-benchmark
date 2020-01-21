68e8bbb54a3d47240b1b07d95d8eed602bc24a03
dfhahn <davidfriedrichhahn@gmail.com>
2020-01-21 20:12:14 +0100

    Update docstrings in edges.py.

--- PLBenchmarks/edges.py
+++ PLBenchmarks/edges.py
@@ -1,7 +1,6 @@
 """
 edges.py
-Protein-Ligand Benchmark Dataset for testing Parameters and Methods of Free Energy Calculations.
-Handles the perturbation edges
+Functions and classes for handling the perturbation edges.
 """
 
 from PLBenchmarks import utils, targets, ligands
@@ -21,14 +20,17 @@ except ImportError:
 
 class edge:
     """
-    Store and convert the data of one perturbation ("edge") in a `pandas.Series`.
-    :param d: dict with the edge data
-    :return None
+    Store and convert the data of one perturbation ("edge") in a :func:`pandas.Series`.
+    
+    :param d: :func:`dict` with the edge data
+    :return: None
     """
+    
     def __init__(self, d: dict):
         """
         Initialize edge class from a dictionary
-        :param d: dict with the edge data
+        
+        :param d: :func:`dict` with the edge data
         :return: None
         """
         self.data = pd.Series(d)
@@ -37,10 +39,15 @@ class edge:
 
     def addLigData(self, ligs):
         """
-        Adds data from ligands to `edge`. Molecule images and the affinity difference are added.
-        :param ligs: ligandSet class of the same target
+        Adds data from ligands to :func:`~PLBenchmarks:edges.edge`. Molecule images and the affinity difference are added.
+        
+        :param ligs: :func:`PLBenchmarks:ligands:ligandSet` class of the same target
         :return: None
         """
+        l0 = None
+        l1 = None
+        dg0 = 0
+        dg1 = 1
         for key, item in ligs.items():
             if key == 'lig_' + str(self.data[0]):
                 l0 = item.data['ROMol'][0][0]
@@ -50,13 +57,14 @@ class edge:
                 dg1 = item.data[('DerivedMeasurement', 'dg')]
         self.data['Mol1'] = l0
         self.data['Mol2'] = l1
-        self.data['exp. DeltaG [kcal/mol]'] = (dg1-dg0).value_in_unit(unit.kilocalories_per_mole).round(2)
+        self.data['exp. DeltaG [kcal/mol]'] = round((dg1-dg0).value_in_unit(unit.kilocalories_per_mole), 2)
 
     def getDF(self, cols=None):
         """
-        Access the edge data as a `pandas.DataFrame`
-        :param cols: list of columns which should be returned in the `pandas.DataFrame`
-        :return: data: `pandas.DataFrame`
+        Access the edge data as a :func:`pandas.DataFrame`
+
+        :param cols: list of columns which should be returned in the :func:`pandas.DataFrame`
+        :return: :func:`pandas.DataFrame`
         """
         if cols:
             return self.data[cols]
@@ -65,15 +73,17 @@ class edge:
 
     def getDict(self):
         """
-        Access the edge data as a dict which contains the name of the edge as key and the names of the two ligands as list.
-        :return: edge: dict
+        Access the edge data as a :func:`dict` which contains the name of the edge as key and the names of the two ligands as :func:`list`.
+        
+        :return: :func:`dict`
         """
         return {f'edge_{self.data[0]}_{self.data[1]}': [f'lig_{self.data[0]}', f'lig_{self.data[1]}']}
 
     def getName(self):
         """
         Access the name of the edge.
-        :return: name: string
+        
+        :return: name as string
         """
         if self._name is not None:
             return self._name
@@ -89,9 +99,10 @@ class edgeSet(dict):
     def __init__(self, target, *arg,**kw):
         """
         Initializes edgeSet class
+        
         :param target: string name of target
-        :param arg: arguments for dict (base class)
-        :param kw: keywords for dict (base class)
+        :param arg: arguments for :func:`dict` (base class)
+        :param kw: keywords for :func:`dict` (base class)
         """
         super(edgeSet, self).__init__(*arg, **kw)
         tp = targets.getTargetDataPath(target)
@@ -105,9 +116,10 @@ class edgeSet(dict):
 
     def getEdge(self, name):
         """
-        Accesses one edge of the edgeSet
+        Accesses one edge of the :func:`PLBenchmarks.edges.edgeSet`
+        
         :param name: string name of the edge
-        :return: edge: edge class
+        :return: :func:`PLBenchmarks:edges:edge` class
         """
         for key in self.keys():
             if key == name:
@@ -118,9 +130,10 @@ class edgeSet(dict):
 
     def getDF(self, columns=None):
         """
-        Access the edgeSet as a `pandas.DataFrame`
-        :param cols: list of columns which should be returned in the `pandas.DataFrame`
-        :return: df: `pandas.DataFrame`
+        Access the :func:`PLBenchmarks:edges.edgeSet` as a :func:`pandas.DataFrame`
+        
+        :param cols: :func:`list` of columns which should be returned in the :func:`pandas.DataFrame`
+        :return: :func:`pandas.DataFrame`
         """
         dfs=[]
         for key, item in self.items():
@@ -130,9 +143,10 @@ class edgeSet(dict):
 
     def getHTML(self, columns=None):
         """
-        Access the edgeSet as a HTML string
-        :param cols: list of columns which should be returned in the `pandas.DataFrame`
-        :return: html: HTML string
+        Access the :func:`PLBenchmarks:edges.edgeSet` as a HTML string
+        
+        :param cols: :func:`list` of columns which should be returned in the :func:`pandas.DataFrame`
+        :return: HTML string
         """
         df = self.getDF(columns)
         html = df.to_html()
@@ -140,8 +154,9 @@ class edgeSet(dict):
 
     def getDict(self):
         """
-        Access the edgeSet as a dict which contains the name of the edges as key and the names of the two ligands in a list as items.
-        :return: res: dict
+        Access the :func:`PLBenchmarks:edges.edgeSet` as a dict which contains the name of the edges as key and the names of the two ligands in a list as items.
+
+        :return: :func:`dict`
         """
         res = {}
         for key, item in self.items():
