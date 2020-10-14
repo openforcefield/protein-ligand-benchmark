Fix std(DG) calculation.

PLBenchmarks/targets.py
@@ -127,11 +127,9 @@ class Target:
         self.ligand_data["minDG"] = round(
             min(affinities) * utils.unit_registry("kcal / mole"), 1
         )
-        # calculation of the mean absolute deviation (mad)
-        mean = np.average(affinities)
-        std = np.std(affinities)        
-        std = std.to("kcal/mole")
-        self.ligand_data["std(DG)"] = round(std, 1)
+        # calculation of the standard deviation
+        std = np.std(affinities)
+        self.ligand_data["std(DG)"] = round(std * utils.unit_registry("kcal / mole"), 1)
 
     def get_ligand_data(self):
         if self.ligand_data is None: