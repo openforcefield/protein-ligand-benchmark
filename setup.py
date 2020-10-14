247660267008881ed7900a375c45daaf491c05c0
dfhahn <davidfriedrichhahn@gmail.com>
2020-10-14 13:20:56 +0200

    Adapt lgtm.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ pytest_runner = ['pytest-runner'] if needs_pytest else []
 try:
     with open("README.md", "r") as handle:
         long_description = handle.read()
-except:
+except IOError::
     long_description = "\n".join(short_description[2:])
 
 
